fb4959be0b17bd3c3d015adaa40adebf9cdaa046
johnbrow10 <iroman250@gmail.com>
2023-03-24 21:08:57 -0300

    feat(cta): adicionando estrutura e formatação do componente CTA

--- src/components/Cta/Cta.jsx
+++ src/components/Cta/Cta.jsx
@@ -3,7 +3,17 @@ import './cta.css'
 
 const Cta = () => {
     return (
-        <div>Cta</div>
+        <div className='gpt3__cta'>
+            <div className="gpt3__cta-container">
+                <p>Solicite acesso antecipado para começar</p>
+                <h3>
+                    Registre-se hoje e comece a explorar as infinitas possibilidades.
+                </h3>
+            </div>
+            <div className='gpt3__cta-btn'>
+                <button type='button'>Comece Agora</button>
+            </div>
+        </div>
     )
 }
 
